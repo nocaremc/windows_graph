a8e5284493a2e7cecdd1a2451624f2a9a3366c7f
Joshua Rodarte <josh@amariconsulting.com>
2017-08-28 09:32:13 -0700

    Setting a variable for path

--- src/Attachment.php
+++ src/Attachment.php
@@ -26,12 +26,12 @@ class Attachment {
     *
     * @return string $file_uri Server Path to the file that was just generated
     */
-    public function saveLocal() {
+    public function saveLocal($file_path) {
 
         $now = new \DateTime();
         $now = $now->format('m-d-Y');
 
-        $file_uri = __DIR__ . "/{$now}-{$this->name}";
+        $file_uri = $file_path . "/{$now}-{$this->name}";
 
         file_put_contents($file_uri, base64_decode($this->contentBytes));
         return $file_uri;
